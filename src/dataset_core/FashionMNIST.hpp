cdd74f14566b717b82a1052197c4b4063d77bb9d
fatorarpolinomio <xextreme225@gmail.com>
2026-05-23 19:36:26 -0300

    feat(dataset_core): pasta nova para códigos referentes ao dataset e esboço de código de treino na main

---
+++ src/dataset_core/FashionMNIST.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <torch/torch.h>
+
+class FashionMNIST : public torch::data::datasets::Dataset<FashionMNIST> {
+public:
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,49 @@
+#include "ATen/core/interned_strings.h"
+#include "dataset_core/FashionMNIST.hpp"
+#include "neural_core/CNN.hpp"
+#include "torch/data/dataloader.h"
+#include "torch/data/samplers/random.h"
+#include "torch/optim/sgd.h"
 #include <iostream>
 #include <matplot/matplot.h>
 #include <torch/torch.h>
 #include <vector>
 
 int main() {
-  std::cout << "Iniciando teste do ambiente..." << std::endl;
 
-  // ---------------------------------------------------------
-  // Usando LibTorch para gerar dados matemáticos
-  // Simulando uma curva de "loss" caindo ao longo de 100 épocas
-  // ---------------------------------------------------------
-  torch::Tensor epocas = torch::linspace(1, 100, 100);
-  // Cria uma curva exponencial decrescente (ex: e^(-0.05 * x))
-  torch::Tensor loss = torch::exp(-0.05 * epocas);
+  // Talvez transformar isso aqui tudo em uma função num arquivo separado
+  const size_t batch_size = 64;
 
-  std::cout << "Tensores gerados com sucesso pelo LibTorch!" << std::endl;
+  // Instanciando o dataset e aplicando a transformação Stack
+  auto dataset = FashionMNIST().map(torch::data::transforms::Stack<>());
 
-  // ---------------------------------------------------------
-  // Ponte de conversão: LibTorch -> Matplot++
-  // O Matplot++ não entende 'torch::Tensor'. Ele precisa de 'std::vector'.
-  // Usamos data_ptr<float>() para extrair os números brutos da memória.
-  // ---------------------------------------------------------
-  std::vector<float> x_dados(epocas.data_ptr<float>(),
-                             epocas.data_ptr<float>() + epocas.numel());
+  // Criando o train_loader (usando RandomSampler pra embaralhar)
+  auto train_loader =
+      torch::data::make_data_loader<torch::data::samplers::RandomSampler>(
+          std::move(dataset), batch_size);
 
-  std::vector<float> y_dados(loss.data_ptr<float>(),
-                             loss.data_ptr<float>() + loss.numel());
+  auto net = NeuralNet();
+  auto loss_function = torch::nn::CrossEntropyLoss();
+  auto optimizer = torch::optim::SGDOptions(0.01).momentum(0.9);
+  auto optimizer_instance = torch::optim::SGD(net.parameters(), optimizer);
 
-  // ---------------------------------------------------------
-  // Usando Matplot++ para desenhar o gráfico
-  // ---------------------------------------------------------
-  std::cout << "Abrindo a janela do gráfico com Matplot++..." << std::endl;
+  for (int epoch; epoch < 30; epoch++) {
+    std::cout << "Treinando epoch: " << epoch << "..." << std::endl;
+    double running_loss = 0.0;
+    for (const auto &[i, data] : c10::prim::enumerate(train_loader)) {
+      auto inputs = data;
+      auto labels = data;
+      optimizer_instance.zero_grad();
+      auto outputs = net(inputs);
+      auto loss = loss_function(outputs, labels);
+      loss.backward();
+      optimizer_instance.step();
+      running_loss += loss.item();
+    }
+    std::cout << "Loss: " << running_loss / train_loader.size() << std::endl;
+  }
 
-  matplot::plot(x_dados, y_dados)->line_width(2).color("blue");
-
-  matplot::title("Simulacao de Loss da CNN");
-  matplot::xlabel("Epocas");
-  matplot::ylabel("Erro (Loss)");
-
-  // Mostra o gráfico em uma janela interativa
-  matplot::show();
-
+  auto model = std::make_shared<NeuralNet>();
+  torch::save(model, "model.pt");
   return 0;
 }
