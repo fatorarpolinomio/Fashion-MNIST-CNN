5592548ad11d18f035afd9e43ba7f97df9d62568
fatorarpolinomio <xextreme225@gmail.com>
2026-05-23 18:57:43 -0300

    feat(CNN.hpp): explicações e links na forma de comentários

--- src/neural_core/CNN.hpp
+++ src/neural_core/CNN.hpp
@@ -1,18 +1,50 @@
 #pragma once
 #include "torch/nn/module.h"
 #include "torch/nn/modules/conv.h"
+#include "torch/nn/modules/linear.h"
 #include "torch/nn/modules/pooling.h"
 #include <torch/torch.h>
 
-class NeuralNet : torch::nn::Module {
+class NeuralNet : public torch::nn::Module {
 private:
+  /* Uma convolução bidimensional é uma operação matemática
+   * que aplica filtros sobrepostos em uma imagem bidimensional,
+   * produzindo um "filtered output", que destaca certas características
+   * da imagem original.
+   * Link:
+   * https://www.geeksforgeeks.org/computer-vision/apply-a-2d-convolution-operation-in-pytorch/
+   * Complemento:
+   * https://medium.com/@ml_dl_explained/understanding-2d-convolutions-in-pytorch-b35841149f5f
+   */
   torch::nn::Conv2d conv1{nullptr}, conv2{nullptr};
+
+  /* Pooling é uma técnica usada para reduzir a amostragem da feature
+   * que vem de camadas anteriores e produz uma nova feature map resumida
+   * Max Pooling seleciona o valor máximo de cada conjunto de filtros
+   * sobrepostos e o passa para a próxima camada
+   * Link:
+   * https://www.geeksforgeeks.org/computer-vision/apply-a-2d-max-pooling-in-pytorch/
+   */
   torch::nn::MaxPool2d pool{nullptr};
+
+  /* Modulo que aplica uma transformação linear sobre a entrada utilizando
+   * pesos e um viés opcional
+   * Link:
+   * https://docs.pytorch.org/docs/2.12/generated/torch.nn.Linear.html */
   torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr}, out{nullptr};
 
 public:
+  NeuralNet();
   NeuralNet(int fc1_dims, int fc2_dims, int fc3_dims);
-  torch::Tensor forward(torch::Tensor x);
+
+  /* Tensor atua como uma matriz multidimensional que armazena os dados
+   * de entrada e saída da rede neural.
+   * Este método é responsável por realizar a propagação para frente (forward)
+   * da rede neural, aplicando as camadas convolucionais, pooling e camadas
+   * totalmente conectadas.
+   * Link:
+   * https://www.geeksforgeeks.org/deep-learning/understanding-the-forward-function-output-in-pytorch/*/
+  torch::Tensor forward(torch::Tensor &x);
 
   // Getters
   torch::nn::Conv2d &get_conv1() { return conv1; }
@@ -20,10 +52,12 @@ public:
   torch::nn::MaxPool2d &get_pool() { return pool; }
   torch::nn::Linear &get_fc1() { return fc1; }
   torch::nn::Linear &get_fc2() { return fc2; }
+  torch::nn::Linear &get_fc3() { return fc3; }
   torch::nn::Linear &get_out() { return out; }
 
   // Setters
   void set_fc1(torch::nn::Linear &fc1) { this->fc1 = fc1; }
   void set_fc2(torch::nn::Linear &fc2) { this->fc2 = fc2; }
+  void set_fc3(torch::nn::Linear &fc3) { this->fc3 = fc3; }
   void set_out(torch::nn::Linear &out) { this->out = out; }
 };
--- src/neural_core/NeuralNet.cpp
+++ src/neural_core/NeuralNet.cpp
@@ -7,9 +7,15 @@
 #include "torch/nn/pimpl.h"
 #include <torch/torch.h>
 
+// Construtor padrão
+NeuralNet::NeuralNet() {}
+
+// Construtor completo da rede neural
 NeuralNet::NeuralNet(int fc1_dims, int fc2_dims, int fc3_dims)
     : conv1(torch::nn::Conv2dOptions(3, 12, 5)),
-      conv2(torch::nn::Conv2dOptions(12, 24, 5)), pool(torch::nn::MaxPool2d()),
+      conv2(torch::nn::Conv2dOptions(12, 24, 5)),
+      pool(torch::nn::MaxPool2d(
+          torch::nn::MaxPool2dOptions({2, 2}).stride({2, 2}))),
       fc1(fc1_dims, fc1_dims), fc2(fc1_dims, fc2_dims), fc3(fc1_dims, fc3_dims),
       out(fc2_dims, 1) {
   register_module("conv1", conv1);
@@ -20,7 +26,8 @@ NeuralNet::NeuralNet(int fc1_dims, int fc2_dims, int fc3_dims)
   register_module("out", out);
 }
 
-torch::Tensor NeuralNet::forward(torch::Tensor x) {
+// Implementação do método forward
+torch::Tensor NeuralNet::forward(torch::Tensor &x) {
   x = this->pool(torch::relu(this->conv1(x)));
   x = this->pool(torch::relu(this->conv2(x)));
   x = torch::flatten(x, 1);
